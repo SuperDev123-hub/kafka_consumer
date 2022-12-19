706440c1ec922fcda595ec4c13cd63c49b8ae0a4
super_dev <s.d19941130@gmail.com>
2022-12-19 02:20:37 -0500

    create producer, consumer

---
+++ src/bin/producer.rs
@@ -0,0 +1,15 @@
+use std::fmt::format;
+
+use kafka::producer::{Producer, Record};
+
+fn main() {
+    let hosts = vec!["localhost:9092".to_owned()];
+
+    let mut producer = Producer::from_hosts(hosts).create().unwrap();
+
+    for i in 0..10 {
+        let buf = format!("{i}abcdef");
+        producer.send(&Record::from_value("topic-name", buf.as_bytes())).unwrap();
+        println!("Sent: {i}");
+    }
+}
